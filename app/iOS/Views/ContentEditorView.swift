BugenZhao/MNGA

commit b0834e6595c9d1a690fc9c7acd2075a9b49bac02

feat(ui): reworked swiftui-native post editor (#166)
* "add tag" for subject field

Signed-off-by: Bugen Zhao <i@bugenzhao.com>

* rework content editor

Signed-off-by: Bugen Zhao <i@bugenzhao.com>

* fix focus and only show corresponding toolbar when focused

Signed-off-by: Bugen Zhao <i@bugenzhao.com>

* rename view

Signed-off-by: Bugen Zhao <i@bugenzhao.com>

---------

Signed-off-by: Bugen Zhao <i@bugenzhao.com>

app/iOS/Views/ContentEditorView.swift
@@ -10,12 +10,50 @@ import Foundation
 import SwiftUI
 import SwiftUIX
 
+enum Focus {
+  case sendTo
+  case subject
+  case content
+}
+
+struct SubjectTextFieldView: View {
+  @Binding var subject: String
+  @State var selection: TextSelection?
+
+  let focused: Bool
+
+  func addTagPlaceholder() {
+    // FIXME: iOS 26: cannot use unicode (localized) string here, use "..." instead for now.
+    let placeholder = "..."
+    subject = "[\(placeholder)]\(subject)"
+    let range = subject.range(of: placeholder)!
+    selection = TextSelection(range: range)
+  }
+
+  var body: some View {
+    TextField("", text: $subject, selection: $selection)
+      .toolbar {
+        // Show toolbar only when focused, otherwise it will also be shown in other text fields.
+        if focused {
+          ToolbarItemGroup(placement: .keyboard) {
+            Button(action: addTagPlaceholder) {
+              Label("Add Tag", systemImage: "tag")
+                .labelStyle(.titleAndIcon)
+            }
+            Spacer()
+          }
+        }
+      }
+  }
+}
+
 struct ContentEditorView<T: TaskProtocol, M: GenericPostModel<T>>: View {
   @Binding var context: M.Context
-  @State var first = true
 
   @StateObject var model: ContentEditorModel
   @StateObject var keyboard = Keyboard.main
+
+  @FocusState var focused: Focus?
 
   @EnvironmentObject var presendAttachments: PresendAttachmentsModel
 
@@ -27,14 +65,29 @@ struct ContentEditorView<T: TaskProtocol, M: GenericPostModel<T>>: View {
 
   @ViewBuilder
   var textEditor: some View {
-    ContentTextEditorView(model: model)
+    ContentTextEditorView(
+      model: model,
+      focused: Binding(get: { focused == .content }, set: { focused = $0 ? .content : nil }),
+    )
   }
 
   @ViewBuilder
   var stickerPanel: some View {
-    StickerInputView(text: $model.text, selected: $model.selected)
+    StickerInputView(model: model)
       .background(.secondarySystemGroupedBackground)
       .frame(maxHeight: 240)
+  }
+
+  func setFocusOnAppear() {
+    focused = if context.to?.isEmpty == true {
+      .sendTo
+    } else if context.subject?.isEmpty == true {
+      .subject
+    } else if context.content?.isEmpty == true {
+      .content
+    } else {
+      nil
+    }
   }
 
   var body: some View {
@@ -44,23 +97,21 @@ struct ContentEditorView<T: TaskProtocol, M: GenericPostModel<T>>: View {
           Section(header: Text("Send To"), footer: Text("Separate multiple users with space.")) {
             TextField("", text: $context.to ?? "")
               .disableAutocorrection(true)
+              .focused($focused, equals: .sendTo)
           }
         }
 
         if context.subject != nil {
           Section(header: Text("Subject")) {
-            TextField("", text: $context.subject ?? "")
+            SubjectEditorView(subject: $context.subject.withDefaultValue(""), focused: focused == .subject)
+              .focused($focused, equals: .subject)
           }
         }
 
         Section(header: Text("Content")) {
-          ZStack(alignment: .topLeading) { // hack for dynamic height
-            textEditor.introspect(.textEditor, on: .iOS(.v14, .v15, .v16, .v17)) { tv in
-              if first { tv.becomeFirstResponder(); first = false }
-            }
-            Text(model.text).opacity(0).padding(.all, 6)
-          }.font(.callout)
+          textEditor
             .frame(minHeight: 250)
+            .focused($focused, equals: .content)
         }
 
         if context.anonymous != nil {
@@ -79,8 +130,10 @@ struct ContentEditorView<T: TaskProtocol, M: GenericPostModel<T>>: View {
         }
       }
     }
+    .onAppear { setFocusOnAppear() }
     .onReceive(keyboard.$isShown) { shown in if shown { model.showing = .none } }
-    .onChange(of: model.text) { context.content = $1 }
+    .onChange(of: model.text) { context.content = model.text }
+    // TODO: use swiftui native photo picker
     .sheet(isPresented: $model.showingImagePicker) { ImagePicker(data: $model.image, encoding: .jpeg(compressionQuality: 0.8)) }
     .onChange(of: model.image) { uploadImageAttachment(data: $1) }
     .toast(isPresenting: $model.image.isNotNil()) { AlertToast(type: .loading) }
@@ -98,7 +151,7 @@ struct ContentEditorView<T: TaskProtocol, M: GenericPostModel<T>>: View {
       context.attachments.append(attachment)
       presendAttachments.add(url: attachment.url, data: data)
 
-      model.insert("\n[img]./\(attachment.url)[/img]")
+      model.insert("\n[img]./\(attachment.url)[/img]\n")
       model.image = nil
     } onError: { _ in
       model.image = nil
@@ -125,4 +178,6 @@ struct ContentEditorView_Previews: PreviewProvider {
     Preview.build(subject: "Subject")
     Preview.build(subject: nil)
   }
+}
+
 }

app/Shared/Localization/zh-Hans.lproj/Localizable.strings
@@ -236,3 +236,5 @@
 "Text Parse" = "文本解析";
 "URL Parse" = "URL 解析";
 "Backend Panic" = "后端错误";
+"Tag" = "标签";
+"Add Tag" = "添加标签";

app/Shared/Models/ContentEditorModel.swift
@@ -22,45 +22,72 @@ class ContentEditorModel: ObservableObject {
     didSet { if showing != .none { keyboard.dismiss() } }
   }
 
-  @Published var selected: NSRange
   @Published var text: String
+  @Published var selection: TextSelection?
 
   @Published var image: Data? = nil
   @Published var showingImagePicker = false
 
-  @objc func showSticker() {
+  func showSticker() {
     showing = .sticker
   }
 
   init(initialText: String) {
-    _text = .init(initialValue: initialText)
-    _selected = .init(initialValue: NSRange(location: (initialText as NSString).length, length: 0))
-  }
-
-  private func appendTag(_ tag: String) {
-    let range = Range(selected, in: text)!
-    let selectedText = text[range]
-    text.replaceSubrange(range, with: "[\(tag)]\(selectedText)[/\(tag)]")
-    let newLocation = selected.location + (tag as NSString).length + 2
-    selected = NSRange(location: newLocation, length: selected.length)
-  }
-
-  @objc func appendBold() {
-    appendTag("b")
-  }
-
-  @objc func appendDel() {
-    appendTag("del")
-  }
-
-  @objc func showImagePicker() {
-    showingImagePicker = true
+    text = initialText
+    selection = TextSelection(insertionPoint: text.endIndex)
   }
 
   func insert(_ string: String) {
-    let range = Range(selected, in: text)!
+    let selection = selection ?? TextSelection(insertionPoint: text.endIndex)
+    guard case let .selection(range) = selection.indices else { return }
+    let prefixLength = text.distance(from: text.startIndex, to: range.lowerBound)
+
     text.replaceSubrange(range, with: string)
-    let newLocation = selected.location + (string as NSString).length
-    selected = NSRange(location: newLocation, length: 0)
+
+    let insertionPoint = text.index(text.startIndex, offsetBy: prefixLength + string.count)
+    self.selection = TextSelection(insertionPoint: insertionPoint)
+
+    // Sometimes it just doesn't work if we set string and selection at the same time...
+    // DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+    //   if let ip = insertionPoint.samePosition(in: self.text) {
+    //     self.selection = TextSelection(insertionPoint: ip)
+    //   }
+    // }
+  }
+
+  private func appendTag(_ tag: String) {
+    let open = "[\(tag)]"
+    let close = "[/\(tag)]"
+
+    let selection = selection ?? TextSelection(insertionPoint: text.endIndex)
+    guard case let .selection(range) = selection.indices else { return }
+    let prefixLength = text.distance(from: text.startIndex, to: range.lowerBound)
+    let distance = text.distance(from: range.lowerBound, to: range.upperBound)
+
+    text.insert(contentsOf: open, at: range.lowerBound)
+    text.insert(contentsOf: close, at: text.index(text.startIndex, offsetBy: prefixLength + open.count + distance))
+
+    // Note: never reuse index after mutation! if there's re-allocation, the index is invalid!
+    let insertionPoint = text.index(text.startIndex, offsetBy: prefixLength + open.count + distance)
+    self.selection = TextSelection(insertionPoint: insertionPoint)
+
+    // Sometimes it just doesn't work if we set string and selection at the same time...
+    // DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+    //   if let ip = insertionPoint.samePosition(in: self.text) {
+    //     self.selection = TextSelection(insertionPoint: ip)
+    //   }
+    // }
+  }
+
+  func appendBold() {
+    appendTag("b")
+  }
+
+  func appendDel() {
+    appendTag("del")
+  }
+
+  func showImagePicker() {
+    showingImagePicker = true
   }
 }

app/Shared/Views/PostContentView.swift
@@ -67,6 +67,7 @@ struct PostContentView<S: Sequence & Equatable>: View where S.Element == Span {
 }
 
 extension PostContentView where S == [Span] {
+  // TODO: shall we increase default font size?
   init(content: PostContent, id: PostId? = nil, postDate: UInt64? = nil, defaultFont: Font = .callout, defaultColor: Color = .primary, initialInQuote: Bool = false) {
     spans = content.spans
     error = content.error

app/Shared/Views/StickerInputView.swift
@@ -9,8 +9,7 @@ import Foundation
 import SwiftUI
 
 struct StickerInputView: View {
-  @Binding var text: String
-  @Binding var selected: NSRange
+  @ObservedObject var model: ContentEditorModel
 
   var body: some View {
     let rows = [GridItem](repeating: .init(.fixed(50)), count: 4)
@@ -33,23 +32,19 @@ struct StickerInputView: View {
   }
 
   func insert(name: String) {
-    let range = Range(selected, in: text)!
     let code = stickerImageNameToCode(name)
-    text.replaceSubrange(range, with: code)
-    let newLocation = selected.location + (code as NSString).length
-    selected = NSRange(location: newLocation, length: 0)
+    model.insert(code)
   }
 }
 
 struct StickerInputView_Previews: PreviewProvider {
   struct Preview: View {
-    @State var text = "233"
-    @State var selected = NSRange()
+    @StateObject var model = ContentEditorModel(initialText: "233")
 
     var body: some View {
       VStack {
-        Text(text)
-        StickerInputView(text: $text, selected: $selected)
+        Text(model.text)
+        StickerInputView(model: model)
           .background(.secondary.opacity(0.2))
       }
     }

app/iOS/Views/ContentTextEditorView.swift
@@ -8,79 +8,49 @@
 import Foundation
 import SwiftUI
 
-struct ContentTextEditorView: UIViewRepresentable {
+struct ContentTextEditorView: View {
   @ObservedObject var model: ContentEditorModel
 
-  init(model: ContentEditorModel) {
-    self.model = model
+  @Binding var focused: Bool
+
+  var body: some View {
+    TextEditor(text: $model.text, selection: $model.selection)
+      .toolbar {
+        // Show toolbar only when focused, otherwise it will also be shown in other text fields.
+        if focused {
+          ToolbarItemGroup(placement: .keyboard) {
+            Button(action: model.showSticker) {
+              Image(systemName: "face.smiling")
+            }
+            Button(action: model.appendBold) {
+              Image(systemName: "bold")
+            }
+            Button(action: model.appendDel) {
+              Image(systemName: "strikethrough")
+            }
+            Button(action: model.showImagePicker) {
+              Image(systemName: "photo")
+            }
+            Spacer()
+            Button(action: hideKeyboard) {
+              Image(systemName: "keyboard.chevron.compact.down")
+            }
+          }
+        }
+      }
   }
 
-  func makeUIView(context: Context) -> UITextView {
-    let textView = UITextView()
-    textView.delegate = context.coordinator
-
-    let inputView = UIInputView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44), inputViewStyle: .keyboard)
-
-    let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44))
-    let flexButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-    let doneButton = UIBarButtonItem(image: UIImage(systemName: "keyboard.chevron.compact.down"), style: .done, target: self, action: #selector(textView.doneButtonTapped(button:)))
-    let stickerButton = UIBarButtonItem(image: UIImage(systemName: "face.smiling"), style: .plain, target: model, action: #selector(ContentEditorModel.showSticker))
-    let imageButton = UIBarButtonItem(image: UIImage(systemName: "photo"), style: .plain, target: model, action: #selector(ContentEditorModel.showImagePicker))
-    let boldButton = UIBarButtonItem(image: UIImage(systemName: "bold"), style: .plain, target: model, action: #selector(ContentEditorModel.appendBold))
-    let delButton = UIBarButtonItem(image: UIImage(systemName: "strikethrough"), style: .plain, target: model, action: #selector(ContentEditorModel.appendDel))
-    let sepButton = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
-    sepButton.width = 12
-
-    toolbar.items = [stickerButton, imageButton, sepButton, boldButton, delButton, flexButton, doneButton]
-
-    // make the toolbar translucent with keyboard
-    toolbar.isTranslucent = true
-    toolbar.setBackgroundImage(UIImage(), forToolbarPosition: .any, barMetrics: .default)
-    toolbar.backgroundColor = .clear
-    inputView.addSubview(toolbar)
-
-    textView.inputAccessoryView = inputView
-    textView.isSelectable = true
-    textView.isUserInteractionEnabled = true
-    textView.font = UIFont.preferredFont(forTextStyle: .callout)
-    textView.backgroundColor = .clear
-
-    return textView
-  }
-
-  func updateUIView(_ textView: UITextView, context _: Context) {
-    let text = model.text
-    let selected = model.selected
-
-    if text != textView.text {
-      textView.text = text
-    }
-    textView.selectedRange = selected
-  }
-
-  func makeCoordinator() -> Coordinator {
-    Coordinator(model: model)
-  }
-
-  class Coordinator: NSObject, UITextViewDelegate {
-    let model: ContentEditorModel
-
-    init(model: ContentEditorModel) {
-      self.model = model
-    }
-
-    func textViewDidChange(_ textView: UITextView) {
-      model.text = textView.text
-    }
-
-    func textViewDidChangeSelection(_ textView: UITextView) {
-      model.selected = textView.selectedRange
+  private func hideKeyboard() {
+    withAnimation {
+      focused = false
     }
   }
 }
 
-extension UITextView {
-  @objc func doneButtonTapped(button _: UIBarButtonItem) {
-    resignFirstResponder()
+// MARK: - Preview
+
+struct ContentTextEditorView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentTextEditorView(model: ContentEditorModel(initialText: "Sample text"), focused: .constant(true))
   }
 }

commit 572028ee436ff4c692bfe698731ca0a822ea9567

fix: fix compilation issue caused by #166
Signed-off-by: Bugen Zhao <i@bugenzhao.com>

app/iOS/Views/ContentEditorView.swift
@@ -103,7 +103,7 @@ struct ContentEditorView<T: TaskProtocol, M: GenericPostModel<T>>: View {
 
         if context.subject != nil {
           Section(header: Text("Subject")) {
-            SubjectEditorView(subject: $context.subject.withDefaultValue(""), focused: focused == .subject)
+            SubjectTextFieldView(subject: $context.subject.withDefaultValue(""), focused: focused == .subject)
               .focused($focused, equals: .subject)
           }
         }
@@ -178,6 +178,4 @@ struct ContentEditorView_Previews: PreviewProvider {
     Preview.build(subject: "Subject")
     Preview.build(subject: nil)
   }
-}
-
 }