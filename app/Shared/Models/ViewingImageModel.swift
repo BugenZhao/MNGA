0e5d625f23ad2724ffcfaba526445e59389f85ed
Bugen Zhao <i@bugenzhao.com>
2025-09-16 23:20:18 +0800

    fix(ui): fix image viewer not showing share button sometimes (#169)
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- app/Shared/Models/ViewingImageModel.swift
+++ app/Shared/Models/ViewingImageModel.swift
@@ -12,26 +12,25 @@ import SwiftUI
 import SwiftUIX
 
 class ViewingImageModel: ObservableObject {
-  @Published var view: AnyView? {
-    didSet { id = UUID() }
-  }
-
   @Published var id: UUID?
+  @Published var view: AnyView?
   @Published var imageData: Data?
   @Published var showing = false
 
   func show(image: PlatformImage) {
     withAnimation {
+      self.imageData = image.sd_imageData()
       self.view = Image(image: image)
         .resizable()
         .eraseToAnyView()
-      self.imageData = image.sd_imageData()
+      self.id = UUID()
       self.showing = true
     }
   }
 
   func show(url: URL) {
     withAnimation {
+      self.imageData = nil
       self.view = WebImage(url: url)
         .onSuccess { image, _, _ in
           DispatchQueue.main.async {
@@ -41,7 +40,7 @@ class ViewingImageModel: ObservableObject {
         .resizable()
         .indicator(.progress)
         .eraseToAnyView()
-      self.imageData = nil
+      self.id = UUID()
       self.showing = true
     }
   }
