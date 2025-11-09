016f3739e510cab465804870a531f4f638ce3661
Bugen Zhao <i@bugenzhao.com>
2025-11-10 00:47:01 +0800

    add example
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

---
+++ read-xml.php
@@ -0,0 +1,302 @@
+<?xml version="1.0" encoding="GB18030"?>
+<root>
+<__CU>
+    <uid>41417929</uid>
+    <group_bit>98528</group_bit>
+    <admincheck></admincheck>
+    <rvrc>15</rvrc>
+</__CU>
+<__GLOBAL>
+    <_ATTACH_BASE_VIEW>img.nga.178.com/attachments</_ATTACH_BASE_VIEW>
+</__GLOBAL>
+<__U>
+    <item>
+        <uid>41417929</uid>
+        <username>BugenZhao</username>
+        <credit>135537153</credit>
+        <medal></medal>
+        <reputation>91_-600,53_-150,-7_-255,61_0</reputation>
+        <groupid>-1</groupid>
+        <memberid>39</memberid>
+        <avatar>https://img.nga.178.com/avatars/2002/cc9/77f/002/41417929_0.jpg?57</avatar>
+        <yz>4</yz>
+        <site></site>
+        <honor></honor>
+        <regdate>1495931441</regdate>
+        <mute_time></mute_time>
+        <postnum>2253</postnum>
+        <rvrc>15</rvrc>
+        <money>7400</money>
+        <thisvisit>1762688273</thisvisit>
+        <signature>[url]http://apple.co/3RwvgIe[/url]&lt;br/&gt;&lt;br/&gt;MNGA 2.0 -
+            现以液态玻璃形态回归 &lt;br/&gt;Make NGA Great Again！</signature>
+        <nickname></nickname>
+        <bit_data>135537153</bit_data>
+    </item>
+    <__GROUPS>
+        <item>
+            <item>学徒</item>
+            <item>98528</item>
+            <item>39</item>
+        </item>
+    </__GROUPS>
+    <__REPUTATIONS>
+        <item>
+            <item>
+                <item>41417929</item>
+                <item>0</item>
+            </item>
+            <name>艾泽拉斯卫士</name>
+            <id>17</id>
+        </item>
+    </__REPUTATIONS>
+</__U>
+<__R>
+<item>
+    <content>测试测试zsbd</content>
+    <alterinfo></alterinfo>
+    <tid>45150945</tid>
+    <score>0</score>
+    <score_2>0</score_2>
+    <postdate>2025-09-18 23:55</postdate>
+    <authorid>41417929</authorid>
+    <subject>[ABF][...][...][...][...][...][...][...][...][...][我是标签ABCDEF] [我是标签]
+        [我是标签] 测试好多标签</subject>
+    <type>33554432</type>
+    <fid>275</fid>
+    <pid>0</pid>
+    <recommend>0</recommend>
+    <follow>0</follow>
+    <lou>0</lou>
+    <content_length>12</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1758210953</postdatetimestamp>
+</item>
+<item>
+    <pid>841045175</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-09-20 14:50</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>[quote][tid=45150945]Topic[/tid] [b]Post by [uid=41417929]BugenZhao[/uid] (2025-09-18
+        23:55):[/b]&lt;br/&gt;&lt;br/&gt;测试测试zsbd[/quote]&lt;br/&gt;&lt;br/&gt;测试回复zsbd</content>
+    <lou>1</lou>
+    <content_length>17</content_length>
+    <from_client>7 iOS</from_client>
+    <postdatetimestamp>1758351000</postdatetimestamp>
+</item>
+<item>
+    <pid>841045434</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-09-20 14:52</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>测试回复zsbd</content>
+    <lou>2</lou>
+    <content_length>12</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1758351168</postdatetimestamp>
+</item>
+<item>
+    <pid>841045735</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-09-20 14:55</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>测试回复zsbdd</content>
+    <lou>3</lou>
+    <content_length>13</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1758351352</postdatetimestamp>
+</item>
+<item>
+    <pid>841045817</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-09-20 14:56</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>测试回复zsbdddd</content>
+    <lou>4</lou>
+    <content_length>15</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1758351397</postdatetimestamp>
+</item>
+<item>
+    <pid>841046175</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-09-20 15:01</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>TestPostingReply</content>
+    <lou>5</lou>
+    <content_length>16</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1758351662</postdatetimestamp>
+</item>
+<item>
+    <pid>841046217</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-09-20 15:01</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>TestPostingReply</content>
+    <lou>6</lou>
+    <content_length>16</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1758351688</postdatetimestamp>
+</item>
+<item>
+    <pid>841046595</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-09-20 15:05</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>TestPostingReply</content>
+    <lou>7</lou>
+    <content_length>16</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1758351927</postdatetimestamp>
+</item>
+<item>
+    <pid>841046783</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-09-20 15:07</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>TestPostingReply</content>
+    <lou>8</lou>
+    <content_length>16</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1758352064</postdatetimestamp>
+</item>
+<item>
+    <pid>846291666</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-11-02 22:35</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>[dice]2d4+2d6+100[/dice]</content>
+    <lou>9</lou>
+    <content_length>20</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1762094140</postdatetimestamp>
+</item>
+<item>
+    <pid>846801800</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-11-06 23:52</postdate>
+    <subject></subject>
+    <alterinfo>[E1762445198 0 0] </alterinfo>
+    <content>[@BugenZhao]&lt;br/&gt;&lt;br/&gt;[@BugenZhao ]&lt;br/&gt;&lt;br/&gt;[@41417929]</content>
+    <lou>10</lou>
+    <content_length>29</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1762444321</postdatetimestamp>
+</item>
+<item>
+    <pid>846917801</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-11-07 23:18</postdate>
+    <subject></subject>
+    <alterinfo></alterinfo>
+    <content>[@BugenZhao]</content>
+    <lou>11</lou>
+    <content_length>10</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1762528699</postdatetimestamp>
+</item>
+<item>
+    <pid>846977351</pid>
+    <fid>275</fid>
+    <tid>45150945</tid>
+    <authorid>41417929</authorid>
+    <type>0</type>
+    <score>0</score>
+    <score_2>0</score_2>
+    <recommend>0</recommend>
+    <postdate>2025-11-08 16:54</postdate>
+    <subject></subject>
+    <alterinfo>[E1762592109 0 0] </alterinfo>
+    <content>[@41417929] &lt;br/&gt;[@BugenZhao]</content>
+    <lou>12</lou>
+    <content_length>19</content_length>
+    <from_client>0 /</from_client>
+    <postdatetimestamp>1762592058</postdatetimestamp>
+</item>
+<item>
+<pid>847017534</pid>
+<fid>275</fid>
+<tid>45150945</tid>
+<authorid>41417929</authorid>
+<type>0</type>
+<score>0</score>
+<score_2>0</score_2>
+<recommend>0</recommend>
+<postdate>2025-11-09 01:36</postdate>
+<su
