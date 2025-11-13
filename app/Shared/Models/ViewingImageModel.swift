40bc205159849e91b35c57439b8df5cc74a909fb
Bugen Zhao <i@bugenzhao.com>
2025-11-13 22:16:00 +0800

    feat: refine image sharing (#274)

--- app/Shared/Models/ViewingImageModel.swift
+++ app/Shared/Models/ViewingImageModel.swift
@@ -11,6 +11,7 @@ import Foundation
 import SDWebImageSwiftUI
 import SwiftUI
 import SwiftUIX
+import UniformTypeIdentifiers
 
 extension URL {
   var hashedFilename: String {
@@ -20,48 +21,49 @@ extension URL {
   }
 }
 
+extension PlatformImage {
+  var utType: UTType? {
+    switch sd_imageFormat {
+    case .JPEG: .jpeg
+    case .PNG: .png
+    case .GIF: .gif
+    case .TIFF: .tiff
+    case .webP: .webP
+    case .HEIC: .heic
+    case .HEIF: .heif
+    case .PDF: .pdf
+    case .SVG: .svg
+    case .BMP: .bmp
+    default: nil
+    }
+  }
+
+  // TODO: for plain image, prefer proxying to `Image`, instead of using `FileRepresentation`
+  var isPlainImage: Bool {
+    if sd_isAnimated { return false }
+
+    switch utType {
+    case .jpeg, .png, .webP: return true
+    default: return false
+    }
+  }
+}
+
 struct TransferableImage: Transferable {
   let image: PlatformImage
-  let ext: String
+  let imageData: Data
+  let utType: UTType
   let url: URL
-  let localURL: URL
 
   init?(url: URL, image: PlatformImage) {
     self.image = image
     self.url = url
 
-    ext = switch image.sd_imageFormat {
-    // static const SDImageFormat SDImageFormatJPEG      = 0;
-    // static const SDImageFormat SDImageFormatPNG       = 1;
-    // static const SDImageFormat SDImageFormatGIF       = 2;
-    // static const SDImageFormat SDImageFormatTIFF      = 3;
-    // static const SDImageFormat SDImageFormatWebP      = 4;
-    // static const SDImageFormat SDImageFormatHEIC      = 5;
-    // static const SDImageFormat SDImageFormatHEIF      = 6;
-    // static const SDImageFormat SDImageFormatPDF       = 7;
-    // static const SDImageFormat SDImageFormatSVG       = 8;
-    // static const SDImageFormat SDImageFormatBMP       = 9;
-    case .JPEG: "jpg"
-    case .PNG: "png"
-    case .GIF: "gif"
-    case .TIFF: "tiff"
-    case .webP: "webp"
-    case .HEIC: "heic"
-    case .HEIF: "heif"
-    case .PDF: "pdf"
-    case .SVG: "svg"
-    case .BMP: "bmp"
-    // use a random extension for fallback
-    default: "png"
-    }
+    guard let imageData = image.sd_imageData() else { return nil }
+    self.imageData = imageData
 
-    localURL = FileManager.default.temporaryDirectory
-      .appendingPathComponent("MNGA-\(url.hashedFilename)")
-      .appendingPathExtension(ext)
-
-    if !FileManager.default.fileExists(atPath: localURL.path) {
-      try? image.sd_imageData()?.write(to: localURL, options: .atomic)
-    }
+    guard let utType = image.utType else { return nil }
+    self.utType = utType
   }
 
   var previewImage: Image {
@@ -69,11 +71,20 @@ struct TransferableImage: Transferable {
   }
 
   var previewName: String {
-    "\(ext.uppercased()) Image (\(url.lastPathComponent))"
+    "\(utType.localizedDescription, default: "Image".localized) @ MNGA"
   }
 
   static var transferRepresentation: some TransferRepresentation {
-    ProxyRepresentation { $0.localURL }
+    FileRepresentation(exportedContentType: .data) {
+      let tempURL = FileManager.default.temporaryDirectory
+        // This method will check the extension from given name and attach the right extension if needed.
+        .appendingPathComponent("MNGA_\($0.url.hashedFilename)_\($0.url.lastPathComponent)", conformingTo: $0.utType)
+
+      if !FileManager.default.fileExists(atPath: tempURL.path) {
+        try $0.imageData.write(to: tempURL, options: .atomic)
+      }
+      return SentTransferredFile(tempURL)
+    }
   }
 }
 
@@ -83,9 +94,12 @@ class ViewingImageModel: ObservableObject {
   @Published var showing = false
 
   func show(url: URL) {
+    transferable = nil
     view = WebImage(url: url).resizable()
       .onSuccess { image, _, _ in
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+          // The constructor will decode image data, which might be expensive.
+          // So we do it in a background thread.
           let transferable = TransferableImage(url: url, image: image)
           DispatchQueue.main.async {
             self?.transferable = transferable
