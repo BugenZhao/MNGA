5831a0d6580d91e819c9e90e1aa874ded9b6acd5
Bugen Zhao <i@bugenzhao.com>
2025-12-06 23:15:22 +0800

    fix clippy
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- logic/service/src/topic.rs
+++ logic/service/src/topic.rs
@@ -196,9 +196,9 @@ pub fn extract_topic(node: Node) -> Option<Topic> {
         get!(map, "topic_misc").and_then(|raw| parse_topic_misc_font_modifier_bits(&raw))
     {
         subject.font_modifiers = FONT_MODIFIER_BITMASKS
-            .iter()
+            .into_iter()
             .filter(|(_, mask)| font_modifier_bits & mask != 0)
-            .map(|(modifier, _)| modifier.clone())
+            .map(|(modifier, _)| modifier)
             .collect();
     }
 
